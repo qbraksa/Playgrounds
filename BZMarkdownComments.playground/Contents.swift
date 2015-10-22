7ba63ce5075c882a4873ddc12feabd7ec2cb5d88
Zakaria Braksa <zakaria.braksa@gmail.com>
2015-10-22 05:11:19 +0100

    init

---
+++ BZMarkdownComments.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+/**
+Describe how special this variable is
+*/
+var str = "Hello, playground"
+
+/**
+Describe what this method does
+- returns: Bool
+- parameter param1:String
+- parameter param2:String
+- Throws: error lists
+*/
+func aMethod(param param: String) -> Bool{
+    return true
+}
+
